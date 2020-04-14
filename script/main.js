ccbc1d03647c0abce5f6090ee2b67cc803aca561
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-14 16:47:27 +0200

    main progress

--- script/main.js
+++ script/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
     console.log('main.js is working')
     console.log($)
-});
+
+
+    const dropdownTitles = $('.dropdown-title')
+    const dropdownContents = $('.dropdown-content')
+    const dropdownWrapper = $('.dropdown-wrapper')
+    const leftBarButtons = $('.leftBar-button')
+    const itLink = $('#it-link')
+
+    dropdownTitles.mouseenter(function() {
+        preventDefault()
+        dropdownContents.hide()
+        $(this).next().show()}
+    )
+
+    $('body').not()
+
+    leftBarButtons.click(function() {
+        leftBarButtons.toggleClass('active');
+    })
+
+    itLink.mouseover(function() {
+        itLink.children('i').toggleClass('fa-angle-down fa-angle-up')
+    }
+    )
+
+
+})
